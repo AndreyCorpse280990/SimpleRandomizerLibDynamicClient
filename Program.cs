6edf63d3185fab19d50a22387f5e73b1d9293322
AndreyCorpse28090 <280990corpse@gmail.com>
2024-07-30 20:38:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRandomizerLibDynamicClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Загрудить сборку dll-библиоетки в текущий проект
+            AssemblyName libAssemlyName = AssemblyName.GetAssemblyName("SimpleRandomizeLib.dll");
+            Assembly libAssembly = Assembly.Load(libAssemlyName);
+            Console.WriteLine($"Loaded lib assemly: {libAssemlyName.FullName}");
+
+            // 2. Загрузить модуль библиотеки
+            Module libModule = libAssembly.GetModule("SimpleRandomizeLib.dll");
+            Console.WriteLine($"Loaded lib module: {libModule.Name}");
+
+        }
+    }
+}
